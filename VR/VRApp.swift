3f5e15988bd30f932d5b35e341fd8cec8259bcba
Vladislav Landa <landacorp@gmail.com>
2023-06-23 16:31:21 +0300

    Initial Commit

---
+++ VR/VRApp.swift
@@ -0,0 +1,17 @@
+//
+//  VRApp.swift
+//  VR
+//
+//  Created by Vladislav Landa on 23/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct VRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
